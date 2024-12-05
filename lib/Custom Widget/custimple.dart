a306677e91b387fa416628a9450331e492066f1a
NormanOsborm <159689265+NormanOsborm@users.noreply.github.com>
2024-12-05 19:06:56 +0530

    secondary commit

--- lib/Custom Widget/custimple.dart
+++ lib/Custom Widget/custimple.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutterpro/Custom%20Widget/customswidget.dart';
-void main(){
-  runApp(MaterialApp(home: Custimple(),debugShowCheckedModeBanner: false,));
-}
-class Custimple extends StatelessWidget {
-  const Custimple({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:Customswidget(card:Card(child: ,), title: null, subtitle: null, onpress: () {  },)
-
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutterpro/Custom%20Widget/customswidget.dart';
+// void main(){
+//   runApp(MaterialApp(home: Custimple(),debugShowCheckedModeBanner: false,));
+// }
+// class Custimple extends StatelessWidget {
+//   const Custimple({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body:Customswidget(card:Card(child: ,), title: null, subtitle: null, onpress: () {  },)
+//
+//     );
+//   }
+// }
